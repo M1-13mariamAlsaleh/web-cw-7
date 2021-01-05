743a4120c0065624fbbb4d47944c370663230c8c
M1-13mariamAlsaleh <meme.alsaleh1@gmail.com>
2021-01-06 01:17:01 +0300

    done

--- question1.js
+++ question1.js
@@ -1,1 +1,11 @@
 // قم بحل السؤال الأول هنا
+let pets =["cats" ,"dogs", "birds","rabbit","hamsters"]
+pets.pop();
+pets.push(`turtles`)
+pets[2] = `fish`
+
+let cats = {catName:"basbosa", catAge:"1year", catType:"british" };
+     cats.emoji=`🐱`
+     console.log(cats.catAge)
+
+     console.log(pets)
